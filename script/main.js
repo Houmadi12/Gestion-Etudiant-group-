41ca1568d1faa74e952ee28bfef08721e9e1994a
Ambdouroihmane Soilihi <ambdousoilihiit@gmail.com>
2024-09-10 08:56:36 +0000

    version 1

--- script/main.js
+++ script/main.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-import { getFirestore, doc, getDocs, addDoc, collection } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBc-g4b8BiwsbBigH9xNilnNEi_1jLSpWI",
-    authDomain: "gestion-etudiants-f98e2.firebaseapp.com",
-    projectId: "gestion-etudiants-f98e2",
-    storageBucket: "gestion-etudiants-f98e2.appspot.com",
-    messagingSenderId: "299493335262",
-    appId: "1:299493335262:web:eddac99292f8584f98224f",
-    measurementId: "G-FFBY6NM6QZ"
+  apiKey: "AIzaSyD6Rc89AeMxmn7U5tLyxc1FF_LjlNGz98s",
+  authDomain: "gestionetudiant-g.firebaseapp.com",
+  projectId: "gestionetudiant-g",
+  storageBucket: "gestionetudiant-g.appspot.com",
+  messagingSenderId: "707935631381",
+  appId: "1:707935631381:web:4fa247ca606dea5551b61c",
+  measurementId: "G-56CH56JP3L"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const db = getFirestore(app);
